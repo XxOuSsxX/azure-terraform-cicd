7552d3580f14aa9c4344520c684d6173b50ffb1a
XxOuSsxX <datakiller@hotmail.com>
2026-02-09 15:02:13 -0500

    Add VNet and subnet

--- main.tf
+++ main.tf
@@ -15,3 +15,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-cicd-test-01"
   location = "Canada Central"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform-cicd"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
