310ab36294779245e89a2ac4f36cb052ab018e2b
XxOuSsxX <datakiller@hotmail.com>
2026-02-09 16:57:55 -0500

    Fix VM size and container image configuration

--- main.tf
+++ main.tf
@@ -15,7 +15,13 @@ provider "azurerm" {
   features {}
 }
 
-# -------- Variables (meilleur que hardcoder) --------
+# -------- Random (évite les conflits de noms) --------
+resource "random_integer" "rand" {
+  min = 10000
+  max = 99999
+}
+
+# -------- Variables --------
 variable "location" {
   type    = string
   default = "East US"
@@ -23,7 +29,7 @@ variable "location" {
 
 variable "vm_size" {
   type    = string
-  default = "Standard_B1ms"
+  default = "Standard_B2s"
 }
 
 variable "admin_username" {
@@ -33,20 +39,29 @@ variable "admin_username" {
 
 variable "admin_ssh_public_key" {
   type        = string
-  description = "SSH public key for VM access"
+  description = "SSH public key for VM access (ex: ssh-rsa AAAA... user@pc)"
 }
 
-
+# -------- Noms uniques --------
+locals {
+  rg_name   = "rg-terraform-cicd-${random_integer.rand.result}"
+  vnet_name = "vnet-terraform-cicd-${random_integer.rand.result}"
+  pip_name  = "pip-vm-terraform-${random_integer.rand.result}"
+  nic_name  = "nic-vm-${random_integer.rand.result}"
+  vm_name   = "vm-terraform-${random_integer.rand.result}"
+  aci_name  = "docker-container-${random_integer.rand.result}"
+  dns_label = "tfc${random_integer.rand.result}"
+}
 
 # -------- Resource Group --------
 resource "azurerm_resource_group" "rg" {
-  name     = "rg-terraform-cicd-test-01"
+  name     = local.rg_name
   location = var.location
 }
 
 # -------- VNet / Subnet --------
 resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-terraform-cicd"
+  name                = local.vnet_name
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
@@ -59,19 +74,18 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-# -------- Public IP (utile pour tester la VM) --------
+# -------- Public IP (Standard) --------
 resource "azurerm_public_ip" "pip" {
-  name                = "pip-vm-terraform"
+  name                = local.pip_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
 
-
 # -------- NIC --------
 resource "azurerm_network_interface" "nic" {
-  name                = "nic-vm"
+  name                = local.nic_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -81,32 +95,26 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.pip.id
   }
-
-  depends_on = [azurerm_subnet.subnet]
 }
 
-# -------- VM Linux --------
-
+# -------- VM Linux (SSH) --------
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-terraform"
+  name                = local.vm_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = var.vm_size
 
-  admin_username = var.admin_username
+  admin_username                  = var.admin_username
+  disable_password_authentication = true
 
-
-
-admin_ssh_key {
-  username   = var.admin_username
-  public_key = var.admin_ssh_public_key
-}
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.admin_ssh_public_key
+  }
 
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
-
-  disable_password_authentication = true
 
   os_disk {
     caching              = "ReadWrite"
@@ -121,36 +129,32 @@ admin_ssh_key {
   }
 }
 
-
-# -------- Random pour DNS du container --------
-resource "random_integer" "rand" {
-  min = 10000
-  max = 99999
-}
-
 # -------- Container (ACI) --------
 resource "azurerm_container_group" "container" {
-  name                = "docker-container"
+  name                = local.aci_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   ip_address_type     = "Public"
-  dns_name_label      = "terraformdocker${random_integer.rand.result}"
+  dns_name_label      = local.dns_label
   os_type             = "Linux"
 
-  container {
-    name   = "nginx"
-    image  = "mcr.microsoft.com/oss/nginx/nginx:1.25.4"
-    cpu    = "0.5"
-    memory = "1.5"
+ container {
+  name   = "nginx"
+  image  = "nginx:latest"
+  cpu    = 0.5
+  memory = 1.5
 
-    ports {
-      port     = 80
-      protocol = "TCP"
-    }
+  ports {
+    port     = 80
+    protocol = "TCP"
   }
 }
 
-# -------- Outputs (pratique pour ton devoir) --------
+# -------- Outputs --------
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
 output "vm_public_ip" {
   value = azurerm_public_ip.pip.ip_address
 }
@@ -158,5 +162,3 @@ output "vm_public_ip" {
 output "container_fqdn" {
   value = azurerm_container_group.container.fqdn
 }
-
-
