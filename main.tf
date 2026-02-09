c1007efe6a08b520e9cef31cb447cabb1311afb3
XxOuSsxX <datakiller@hotmail.com>
2026-02-09 16:12:33 -0500

    Add admin_ssh_public_key variable

--- main.tf
+++ main.tf
@@ -89,9 +89,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username = var.admin_username
 
 variable "admin_ssh_public_key" {
-  type      = string
-  sensitive = true
+  type        = string
+  description = "SSH public key for the VM admin user"
 }
+
 
 admin_ssh_key {
   username   = var.admin_username
