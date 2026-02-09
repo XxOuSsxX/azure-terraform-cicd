052b089c75fb07b8b9e17ad6040e130b0f46ff34
XxOuSsxX <datakiller@hotmail.com>
2026-02-09 15:20:10 -0500

    Change VM size to B2s

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                = "vm-terraform"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
+  size                = "Standard_B2s"
   admin_username      = "azureuser"
 
   network_interface_ids = [
