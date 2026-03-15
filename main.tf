127f1c9942a6a2f61f54f87e9f0428073b628ef1
XxOuSsxX <xxdatakillerxx@outlook.com>
2026-03-15 16:33:36 -0400

    add virtual network

--- main.tf
+++ main.tf
@@ -6,3 +6,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-cicd"
   location = "Canada Central"
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-terraform-cicd"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.0.0.0/16"]
+}
