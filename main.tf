2133c04ccac0deaf186853f0e4344477a934f8e9
XxOuSsxX <xxdatakillerxx@outlook.com>
2026-03-15 15:56:16 -0400

    step 7 resource group

--- main.tf
+++ main.tf
@@ -6,22 +6,3 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-cicd"
   location = "Canada Central"
 }
-
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "vm-terraform-cicd"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_B1s"
-
-  admin_username = "azureuser"
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = var.admin_ssh_public_key
-  }
-}
-
-variable "admin_ssh_public_key" {
-  description = "SSH public key for the VM"
-  type        = string
-}
