8ab8f8340ed6eb6985bb5082deabc90b02408185
XxOuSsxX <xxdatakillerxx@outlook.com>
2026-03-15 15:53:14 -0400

    add ssh variable

--- main.tf
+++ main.tf
@@ -6,3 +6,17 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-terraform-cicd"
   location = "Canada Central"
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-terraform-cicd"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+
+  admin_username = "azureuser"
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = var.admin_ssh_public_key
+  }
+}
